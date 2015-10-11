fffe240a9f4eb57aaf95246d515aab1045ed5648
originalpixel <bianca.k.01@gmail.com>
2015-10-11 19:03:46 +0200

    update

---
+++ Dotzdesign/lightbox/js/_vti_cnf/lightbox.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Mar 2013 09:07:46 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|test.html test3.html lightbox/index.html
+vti_author:SR|Bianca-PC\\Bianca
+vti_modifiedby:SR|Bianca-PC\\Bianca
+vti_timecreated:TR|21 Apr 2012 10:51:16 -0000
+vti_nexttolasttimemodified:TR|19 Mar 2013 09:07:47 -0000
+vti_cacheddtm:TX|19 Mar 2013 09:08:17 -0000
+vti_filesize:IR|11966
